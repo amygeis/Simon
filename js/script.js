251340c4666f1cd848a3a4a4b5767e1487ed50c1
Amy Lawrenz <lawrenzamym@johndeere.com>
2020-07-10 15:40:43 -0500

    can click buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,55 @@
-console.log("hello")
+
+//need button class and 4 buttons
+
+
+
+class Player{
+    constructor(){
+        this.level = level;
+
+    }
+    score = () =>{
+
+    }    
+}
+
+class Button{
+    constructor(color){
+        this.color = color;
+        this.bright = false;
+        this.click = document.querySelector(`.${this.color}`).addEventListener("click",this.click)
+    }
+    click = () => {
+        console.log(this.color)
+
+    }
+    flash = () => {
+
+    }
+}
+
+const red =new Button("red");
+const yellow = new Button("yellow");
+const green = new Button("green");
+const blue = new Button("blue");
+
+//need game sequence class
+
+class Sequence{
+    constructor(){
+        this.pattern=pattern;
+    }
+    newButton = () => {
+
+    }
+}
+//need to evaluate play
+
+
+
+//need timer
+
+
+//need random generator
+
+
