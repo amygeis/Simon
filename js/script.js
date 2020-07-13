7d5ad826171d88f21b120656a9a5efc1745b668a
Amy Lawrenz <lawrenzamym@johndeere.com>
2020-07-13 08:59:07 -0500

    started creating sequence of play

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 //need button class and 4 buttons
-
-
+const startButton = document.querySelector(`#start`)
+const startClick = startButton.addEventListener("click", startGame)
 
 class Player{
     constructor(){
@@ -17,14 +17,35 @@ class Button{
     constructor(color){
         this.color = color;
         this.bright = false;
-        this.click = document.querySelector(`.${this.color}`).addEventListener("click",this.click)
+        this.button = document.querySelector(`.${this.color}`)
+        this.click = this.button.addEventListener("click",this.click)
     }
     click = () => {
-        console.log(this.color)
+        this.flash();
 
     }
     flash = () => {
-
+        console.log(this.button);
+        if (this.color == "red") {
+            this.button.style.borderTop = "200px solid lightcoral";
+        } else {
+            console.log(this.color);
+            let color = `light${this.color}`;
+            console.log(color);
+            this.button.style.borderTop = "200px solid " + color;
+        }
+        
+        const darkcolor = function () {
+            console.log(this.button)
+            this.button.style.borderTop = "200px solid " + this.color;
+        }
+        setTimeout (darkcolor, 500) 
+    }
+    playSequence = () => {
+        for(let i=0;i<this.pattern.length;i++){
+            this.pattern[i].flash
+            //wait
+        }
     }
 }
 
@@ -35,14 +56,27 @@ const blue = new Button("blue");
 
 //need game sequence class
 
+function startGame(e){
+    e.preventDefault();
+    while(compSequence.pattern = player1.pattern){
+        compSequence.pattern.push(nextSequence)
+        compSequence.playSequence
+
+    }
+}
+
 class Sequence{
     constructor(){
-        this.pattern=pattern;
+        this.pattern=[];
     }
     newButton = () => {
 
     }
 }
+
+const compSequence = new Sequence()
+const player1 = new Sequence()
+
 //need to evaluate play
 
 
@@ -51,5 +85,23 @@ class Sequence{
 
 
 //need random generator
+function randomColor(){
+    let nextSequence = Math.floor(Math.random()*4)
+        switch(nextSequenc) {
+            case 0:
+                nextSequence="red";
+                break;
+            case 1:
+                nextSequence="yellow";
+                break;
+            case 2:
+                nextSequence="green";
+                break;
+            case 3:
+                nextSequence="blue";
+                break;
+        }
+    return nextSequence;
+}
 
 
