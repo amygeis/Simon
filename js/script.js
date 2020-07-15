198845d6e0da63576e3d7c5a479d60cf3ecafed7
Amy Lawrenz <lawrenzamym@johndeere.com>
2020-07-15 10:58:36 -0500

    timeout on response working

--- js/script.js
+++ js/script.js
@@ -39,40 +39,32 @@ class Button{
     }
     click = () => {
         //clearTimeout(timer)   //silver
-        // if(player1.turn=false){
-        //     return;
-        // }
+        //timer = setTimeout(player1.lose,5000)
         this.flash();
-        //console.log(this.color)
         player1.pattern.push(this.color)
         
         if(evaluate()){
-           // timer = setTimeout(player1.lose,5000) //silver level - timeout on user input
+            //clearTimeout(timer) 
+           //timer = setTimeout(player1.lose,5000) //silver level - timeout on user input
         } else {
-            // console.log(`evaluate false=${evaluate}`)
             player1.lose()
         }
         if(player1.pattern.length == compSequence.pattern.length){
-            console.log("length matches")
             clearTimeout(timer)
-            console.log(player1.pattern)
             player1.pattern=[];
             player1.turn=false;
         }
         return;
     }
     flash = () => {
-        //console.log("flash")
         playSound(this.color)
         if (this.color == "red") {
             this.button.style.borderTop = "200px solid lightcoral";
         } else {
-            //console.log(this.color);
             let color = `light${this.color}`;
-            //console.log(color);
             this.button.style.borderTop = "200px solid " + color;
         }
-        setTimeout (this.darkcolor, 500) 
+        setTimeout (this.darkcolor, 420) 
     }
     darkcolor =() => {
         this.button.style.borderTop = "200px solid " + this.color;
@@ -99,11 +91,12 @@ async function startGame(e){
         let newColor = compSequence.randomColor();
         compSequence.pattern.push(newColor)
         playSequence(compSequence.pattern,1000)
-        //timer = setTimeout(player1.lose,5000) //silver
+        timer = setTimeout(player1.lose,5000+l*500) //silver
         player1.turn=true;
         while(player1.turn){
             await sleep(1000)
             }
+            clearTimeout(timer)
     };
     if(player1.winning = false){
                     player1.lose()
@@ -120,9 +113,7 @@ class Sequence{
         this.winning=true;
         this.level=0;
     }
-    newButton = () => {
 
-    }
     randomColor = () => {
         let nextSequence = Math.floor(Math.random()*4)
         switch(nextSequence) {
@@ -149,13 +140,10 @@ class Sequence{
         for(let i=0;i<this.pattern.length;i++){
             let color = this.pattern[i];
             setTimeout (`${color}.flash()`, 1000); 
-            //wait
-
         }
     }
     evaluate = () =>{
         for(let i=0;i<this.pattern.length;i++){
-            //console.log(`player1 ${player1.pattern[i]} comp ${compSequence.patter[i]}`)
             if (player1.pattern[i] == compSequence.pattern[i]){
                 //return true;
             } else {
@@ -167,15 +155,17 @@ class Sequence{
     }
     win =() =>{
         selectedLevelList.disabled=false;
+        player1.pattern=[];
         startButton.disabled=false;
         alert(`"You won!!!  You passed all ${selectedLevel} rounds!"`)
         let wincolor = compSequence.pattern[compSequence.pattern.length-1]
         for (let i=0;i<6;i++){
-            setTimeout(`${wincolor}.flash()`, i*750)
+            setTimeout(`${wincolor}.flash()`, i*650)
         }
         
     }
     lose =() => {
+        player1.pattern=[];
         console.log("player 1 loses")
         alert("Sorry!!  You clicked the wrong color!")
         this.winning=false;
@@ -186,7 +176,6 @@ class Sequence{
 
 function evaluate () {
     for(let i=0;i<player1.pattern.length;i++){
-        //console.log(`player1 ${player1.pattern[i]} comp ${compSequence.pattern[i]}`)
         if (player1.pattern[i] != compSequence.pattern[i]){
             return false;
             break
@@ -204,10 +193,11 @@ function sleep(ms) {
 async function playSequence(sequence, time) {
     for(let i=0;i<sequence.length;i++){
         let color = sequence[i];
-        await sleep(1000)
+        await sleep(500)
         setTimeout (`${color}.flash()`, 500); 
-        //wait
-
+    }
+    if(player1.turn){
+        //timer = setTimeout(player1.lose,5000)
     }
 }
 
@@ -216,4 +206,3 @@ const player1 = new Sequence()
 
 
 let timer;
-
